303dd1d1c29912e2a61ca4dd3046dd7f08a6ea8b
Carl Farterson <carlfarterson@pm.me>
2020-08-22 20:10:29 -0500

    Start creating build_tx()

--- karma/main.py
+++ karma/main.py
@@ -12,6 +12,30 @@ import requests
 # Variables
 ADDRESS_KARMA = "0xdfe691F37b6264a90Ff507EB359C45d55037951C"
 recipients = ["0x2fd20232d72607ac54b756cf8d88f5c0792d9c6c","0xde71d7d06855b29eec3513393ddefd5f503ecbd0"]
+transaction = {
+    'to': '',
+}
+
+
+def build_tx(_from, to, gasPrice, nonce, value=0, data=None, **kwargs):
+
+    # Handle Hexadecimal entries
+    if type(_from) == "int":
+        _from = hex(_from)
+
+    if type(to) == "int":
+        to = hex(int)
+
+    # Make sure gasPrice is converted to Ether
+
+    tx = {
+        **kwargs,
+        'to': to,
+        'gasPrice': gasPrice
+    }
+
+
+    tx = {}
 
 # Load config file
 with open("config/config.yaml", "r") as stream:
@@ -30,6 +54,8 @@ def main(mainnet=False):
 
     for recipient in recipients:
         pass
+
+
 
 
 
