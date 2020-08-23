bd6a103d2896702ebe0a242440d76d0a5de917f6
Carl Farterson <carlfarterson@pm.me>
2020-08-22 19:19:35 -0500

    Create some initial variables

--- karma/main.py
+++ karma/main.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import Web3
 
+# Variables
+ADDRESS_KARMA = "0x123"
+recipients = ["0x2fd20232d72607ac54b756cf8d88f5c0792d9c6c","0xde71d7d06855b29eec3513393ddefd5f503ecbd0"]
+
+
+
 
 def main():
-    return
+    for recipient in recipients:
+        pass
+
+
 
 if __name__ == "__main__":
     main()
