b3e57a388e19ec8c1deaba3f9c24926a968a0b88
Carl Farterson <carlfarterson@pm.me>
2020-08-22 19:29:13 -0500

    Add KARMA contract address

--- karma/main.py
+++ karma/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import Web3
 
 # Variables
-ADDRESS_KARMA = "0x123"
+ADDRESS_KARMA = "0xdfe691F37b6264a90Ff507EB359C45d55037951C"
 recipients = ["0x2fd20232d72607ac54b756cf8d88f5c0792d9c6c","0xde71d7d06855b29eec3513393ddefd5f503ecbd0"]
 
 
