be389458e6d751c98b114aab9ad0f16d1b4c8b90
Carl Farterson <carlfarterson@pm.me>
2020-08-25 20:26:29 -0500

    Improve docs

--- karma/main.py
+++ karma/main.py
@@ -7,14 +7,27 @@ import json
 import requests
 
 
-
-
 # Variables
 ADDRESS_KARMA = "0xdfe691F37b6264a90Ff507EB359C45d55037951C"
 recipients = ["0x2fd20232d72607ac54b756cf8d88f5c0792d9c6c","0xde71d7d06855b29eec3513393ddefd5f503ecbd0"]
 transaction = {
     'to': '',
 }
+
+
+# Load config file
+with open("config/config.yaml", "r") as stream:
+    config = yaml.safe_load(stream)
+
+
+
+# Connect to HTTPProvider
+if mainnet:
+    web = Web3(Web3.HTTPProvider(config['INFURA']['RINKEBY']))
+else:  # We're on Rinkeby
+    web = Web3(Web3.HTTPProvider(config['INFURA']['MAINNET']))
+
+
 
 
 def build_tx(_from, to, gasPrice, nonce, value=0, data=None, **kwargs):
@@ -37,19 +50,8 @@ def build_tx(_from, to, gasPrice, nonce, value=0, data=None, **kwargs):
 
     tx = {}
 
-# Load config file
-with open("config/config.yaml", "r") as stream:
-    config = yaml.safe_load(stream)
-
-
 
 def main(mainnet=False):
-
-    # Connect to HTTPProvider
-    if mainnet:
-        web = Web3(Web3.HTTPProvider(config['INFURA']['RINKEBY']))
-    else:  # We're on Rinkeby
-        web = Web3(Web3.HTTPProvider(config['INFURA']['MAINNET']))
 
 
     for recipient in recipients:
